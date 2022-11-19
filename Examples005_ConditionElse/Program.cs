130ba00a750aebe159890326d50eb78c93fc129c
oxanakapcova <sakhaoks@hotmail.com>
2022-11-19 20:53:23 +0300

    add Examples005 Condition if else

---
+++ Examples005_ConditionElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("username");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "max")
+{
+    Console.WriteLine("Wow, yo're Max!");
+}
+else
+{
+    Console.Write("Hi,");
+    Console.Write(username);
+}
