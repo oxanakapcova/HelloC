2f511bc55480dcd2baf05286afd14824eec37dad
oxanakapcova <sakhaoks@hotmail.com>
2022-11-20 15:23:52 +0300

    добавлено инфо о блоках и инструкциях в Examples001

---
+++ Examples008_RunDog/Program.cs
@@ -0,0 +1,30 @@
+﻿int counte = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int firstFriendSpeed = 2;
+int time = 0;
+
+if distance <= 10
+{
+    Console.Write(count);
+}
+else
+{
+    if friend = 1 
+    time = distance/(firstFriendSpeed + secondFriendSpeed);
+}
+else
+{
+    time = distance/(secondFriendSpeed + dogSpeed);
+}
+distance = distance(firstFriendSpeed + secondFriendSpeed) * time;
+
+count++;
+
+{
+    Console.Write(counte);
+}
+
+
