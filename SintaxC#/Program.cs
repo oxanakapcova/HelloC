6543532cac55b4767990aaa87d78dd2e5093e79a
oxanakapcova <sakhaoks@hotmail.com>
2022-11-20 19:50:11 +0300

    add SintaxC#

---
+++ SintaxC#/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine(true); //логический литерал Правда
+Console.WriteLine(false); //логический литерал Ложь
+// литерал это как константа неизменяемое
+//null отсутствие значения
+Console.WriteLine(-11); //целые десятичные числа
+Console.WriteLine(5);
+Console.WriteLine(505);
+Console.WriteLine(0b11); //числа в двоичной системе вначале пишем 0b
+Console.WriteLine(0b1011);//11
+Console.WriteLine(0b100001);//33
+Console.WriteLine(0x0A); //числа в шестнадцатиричной системе вначале пишем 0x
+Console.WriteLine(0xFF);//255
+Console.WriteLine(0xA1);//161
+Console.WriteLine("Company \"Horns and Hooves\"");//чтобы внутри строки вывести "" отделяем обратным слэшем
+Console.WriteLine("Hello \nworld!");//последовательность \n перенесет следующее слово на новую строчку
